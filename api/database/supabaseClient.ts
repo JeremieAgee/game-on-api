2302efb1407477ca6ae20c961cde40a658d51a82
JeremieAgee <you@example.com>
2024-10-23 22:28:06 -0500

    updated for if there is no url/key

--- api/database/supabaseClient.ts
+++ api/database/supabaseClient.ts
@@ -1,8 +1,9 @@
 // supabaseClient.js
-const { createClient } = require("@supabase/supabase-js");
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_KEY;
+
+const supabaseUrl = process.env.SUPABASE_URL?? "";
+const supabaseAnonKey = process.env.SUPABASE_KEY?? "";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
