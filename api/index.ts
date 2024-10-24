1d88f3b55483572ba9e08c33757f0ea85546fd59
JeremieAgee <you@example.com>
2024-10-24 14:34:55 -0500

    testing for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,3 @@
-
 // server.js
 import { Site } from "./utils/site";
 const express = require("express");
@@ -8,34 +7,33 @@ import authenticateUser from "./middleware/authMiddleware";
 const thisSite = new Site("GameON");
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL, // Replace with your client's origin
-    // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+	origin: process.env.CLIENT_URL, // Replace with your client's origin
+	// Allowed HTTP methods
+	allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 };
 // Initialize Express app
 const app = express();
 // Middleware
-(async()=>{
-    await thisSite.setSite();
-app.use(cors(corsOptions));
-app.use(express.json());
+(async () => {
+	await thisSite.setSite();
+	app.use(cors(corsOptions));
+	app.use(express.json());
 
-// Routesa
-app.use(authenticateUser);
+	// Routesa
+	app.use(authenticateUser);
 
-app.get("/games", thisSite.getAllGames);
-app.get("/games/genre/:id", thisSite.getGamesByGenre);
-app.get("/games/platorms/:id", thisSite.getGamesByPlatform);
+	app.get("/games", thisSite.getAllGames);
+	app.get("/games/genre/:id", thisSite.getGamesByGenre);
+	app.get("/games/platorms/:id", thisSite.getGamesByPlatform);
 
-app.get("/genre", thisSite.getAllGenre);
-app.get("/platforms", thisSite.getAllPlatforms);
+	app.get("/genre", thisSite.getAllGenre);
+	app.get("/platforms", thisSite.getAllPlatforms);
 
-app.get("/tournaments", thisSite.getAllTournaments);
-app.post("/tournaments", thisSite.addTournament);
-app.put("/tournaments/:id", thisSite.updateTournament);
-app.put("/tournaments/:id/player", thisSite.addPlayerToTournament);
-app.delete("/tournaments/:id/player", thisSite.removePlayerFromTournament);
-app.delete("/tournaments/:id", thisSite.deleteTournament);
-
-
-module.exports = app;})();
+	app.get("/tournaments", thisSite.getAllTournaments);
+	app.post("/tournaments", thisSite.addTournament);
+	app.put("/tournaments/:id", thisSite.updateTournament);
+	app.put("/tournaments/:id/player", thisSite.addPlayerToTournament);
+	app.delete("/tournaments/:id/player", thisSite.removePlayerFromTournament);
+	app.delete("/tournaments/:id", thisSite.deleteTournament);	
+})();
+module.exports = app;
--- api/utils/siteClasses.ts
+++ api/utils/siteClasses.ts
@@ -21,7 +21,6 @@ export class Game{
     id: number;
     title: string;
     genreId: number;
-
     constructor(title: string, genreId: number, id?: number){
         this.id = id?? 0;
         this.title = title;
