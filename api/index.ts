8d74174303bcd181d1db4439b29a99769ca92345
JeremieAgee <you@example.com>
2024-10-24 14:37:38 -0500

    trial/error

--- api/index.ts
+++ api/index.ts
@@ -16,24 +16,24 @@ const app = express();
 // Middleware
 (async () => {
 	await thisSite.setSite();
-	app.use(cors(corsOptions));
-	app.use(express.json());
-
-	// Routesa
-	app.use(authenticateUser);
-
-	app.get("/games", thisSite.getAllGames);
-	app.get("/games/genre/:id", thisSite.getGamesByGenre);
-	app.get("/games/platorms/:id", thisSite.getGamesByPlatform);
-
-	app.get("/genre", thisSite.getAllGenre);
-	app.get("/platforms", thisSite.getAllPlatforms);
-
-	app.get("/tournaments", thisSite.getAllTournaments);
-	app.post("/tournaments", thisSite.addTournament);
-	app.put("/tournaments/:id", thisSite.updateTournament);
-	app.put("/tournaments/:id/player", thisSite.addPlayerToTournament);
-	app.delete("/tournaments/:id/player", thisSite.removePlayerFromTournament);
-	app.delete("/tournaments/:id", thisSite.deleteTournament);	
 })();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Routesa
+app.use(authenticateUser);
+
+app.get("/games", thisSite.getAllGames);
+app.get("/games/genre/:id", thisSite.getGamesByGenre);
+app.get("/games/platorms/:id", thisSite.getGamesByPlatform);
+
+app.get("/genre", thisSite.getAllGenre);
+app.get("/platforms", thisSite.getAllPlatforms);
+
+app.get("/tournaments", thisSite.getAllTournaments);
+app.post("/tournaments", thisSite.addTournament);
+app.put("/tournaments/:id", thisSite.updateTournament);
+app.put("/tournaments/:id/player", thisSite.addPlayerToTournament);
+app.delete("/tournaments/:id/player", thisSite.removePlayerFromTournament);
+app.delete("/tournaments/:id", thisSite.deleteTournament);
 module.exports = app;
